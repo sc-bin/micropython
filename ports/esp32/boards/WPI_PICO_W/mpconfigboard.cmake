a47b37a32dc4d60894b46109a7cdfe10daf79d08
sc-bin <3335447573@qq.com>
2024-07-18 15:01:19 +0800

    增加pico的编译配置

---
+++ ports/esp32/boards/WPI_PICO_W/mpconfigboard.cmake
@@ -0,0 +1,13 @@
+set(IDF_TARGET esp32s3)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.usb
+    boards/sdkconfig.ble
+    boards/sdkconfig.240mhz
+    boards/sdkconfig.spiram_sx
+    boards/sdkconfig.spiram_oct
+    boards/WPI_PICO_W/sdkconfig.board
+)
+
+set(MICROPY_PORT_EN_ESPAI y) #AI
---
+++ ports/esp32/boards/WPI_PICO_W/mpconfigboard.h
@@ -0,0 +1,18 @@
+#define MICROPY_HW_BOARD_NAME               "Walnut Pi Pico W"
+#define MICROPY_HW_MCU_NAME                 "ESP32-S3"
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "WPI_PICO_W"
+
+#define MICROPY_PY_MACHINE_DAC              (0)
+
+#define MICROPY_HW_I2C0_SCL                 (9)
+#define MICROPY_HW_I2C0_SDA                 (8)
+
+#define MICROPY_HW_ESPAI				(1)
+#define MICROPY_ENABLE_FACE_DETECTION	(1)
+// #define MICROPY_ENABLE_CAT_DETECTION	(1)
+// #define MICROPY_ENABLE_COLOR_DETECTION	(1)
+// #define MICROPY_ENABLE_CODE_RECOGNITION	(1)
+// #define MICROPY_ENABLE_MOTION_DETECTION	(1)
+// #define MICROPY_ENABLE_FACE_RECOGNITION (1)
+
+
