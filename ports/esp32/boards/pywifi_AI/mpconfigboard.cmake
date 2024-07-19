9c0eacef9ad43d5eaeee6f582f985e55dd769727
sc-bin <3335447573@qq.com>
2024-07-19 13:43:33 +0800

    esp32: 创建pywifi编译项

--- ports/esp32/boards/pywifi_AI/mpconfigboard.cmake
+++ ports/esp32/boards/pywifi_AI/mpconfigboard.cmake
@@ -2,11 +2,11 @@ set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
-    boards/sdkconfig.usb
     boards/sdkconfig.ble
     boards/sdkconfig.240mhz
     boards/sdkconfig.spiram_sx
     boards/sdkconfig.spiram_oct
-    boards/WPI_PICO_W/sdkconfig.board
+    boards/pywifi_AI/sdkconfig.board
 )
 
+set(MICROPY_PORT_EN_ESPAI y) #AI
